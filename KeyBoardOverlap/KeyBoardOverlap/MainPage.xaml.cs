5a6dc0030cb6059f64575942a86ed0b9ab9d05a3
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2024-04-29 15:26:29 +0530

    Keyboard Overlap sample

---
+++ KeyBoardOverlap/KeyBoardOverlap/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace KeyBoardOverlap
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void TransferOutClicked_Clicked(object sender, EventArgs e)
+        {
+            contentView.TranslationX = DeviceDisplay.Current.MainDisplayInfo.Width - 970;
+        }
+
+        private void TransferTo0_Clicked(object sender, EventArgs e)
+        {
+            contentView.TranslationX = 0;
+        }
+    }
+
+}
